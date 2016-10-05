5ca4bd5faed3c3475ae2e3b2861cedb2fc6c393c
davidPies <2014119@edtools.psd401.net>
2016-10-05 12:19:18 -0700

    Create ideas.java

---
+++ ideas.java
@@ -0,0 +1,7 @@
+//Armor:  hair(different versons/trump styles), bullet-proof vest, 
+
+//trump mob / other canidates(weaker than trump)
+
+//print death by trump: get trumped
+
+//Item:  Electionstuff
